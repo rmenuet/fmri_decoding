a7fcf6b136e2073f47902e5c8445400ca986aa7b
Raphael Meudec <raphael.meudec@inria.fr>
2021-02-09 18:07:07 +0100

    Clearer collect script

--- Experiment/a1_collect.py
+++ Experiment/a1_collect.py
@@ -14,70 +14,17 @@ from pathlib import Path
 # 3rd party modules
 import argparse
 import json
-import pickle
 import pandas as pd
 from nilearn.datasets import fetch_neurovault
 
 
-# ================================
-# === FETCHING FROM NEUROVAULT ===
-# ================================
-def fetch_nv(repo, nv_file,
-             download=False,
-             verbose=False,
-             mode="download_new"):
-    """
-    Loads neurovault into memory, either downloading it from the web-API or
-    loading it from the disk.
-
-    :param repo: str
-        Path where the data is downloaded.
-    :param nv_file: str
-        Pickle file where the full data is saved
-        (for faster loading than the fetch_neurovault).
-    :param download: bool, default=False
-        If True: the data is downloaded from the web-API.
-    :param verbose: bool, default=False
-        Activate verbose mode.
-
-    :return: Bunch
-        A dict-like object containing the data from fMRIs fetched from
-        Neurovault.
-    """
-
-    # Download and save to disk or load from disk
-    if download:
-        if verbose:
-            print("...Download from Neurovault API...")
-
-
-        with open(nv_file, 'wb+') as f:
-            pickle.dump(neurovault, f)
-    else:
-        if verbose:
-            print("...Load pre-fetched data from Neurovault...")
-        with open(nv_file, 'rb') as f:
-            neurovault = pickle.load(f)
-
-    n_fmri_dl = len(neurovault.images)
-    if verbose:
-        print("  > Number of (down)loaded fmri =", n_fmri_dl)
-
-    return neurovault
-
-
-def load_colls(neurovault,
-               colls_file,
-               verbose=False):
+def load_colls(neurovault, verbose=False):
     """
     Loads deduplicated (1 row per collection instead of 1 per fMRI) collections
     metadata into a dataframe
 
-    :param nv_file: str
-        Pickle file where the full data is saved
-        (for faster loading than the fetch_neurovault).
-    :param colls_file: str
-        CSV file  where to save collections metadata.
+    :param neurovault: nilearn.utils.Bunch
+        Bunch variable returned by the fetch_neurovault from nilearn
     :param verbose: bool, default=False
         Activates verbose mode for troubleshooting.
 
@@ -87,8 +34,10 @@ def load_colls(neurovault,
     # Build a dataframe from the fetcher's collections metadata
     # (1 row per fMRI)
     colls = pd.DataFrame(neurovault.collections_meta)
-    colls.set_index("id", inplace=True)
-    colls.sort_index(inplace=True)
+    colls = (
+        colls.set_index("id")
+        .sort_index()
+    )
 
     # Compute the number of downloaded fMRIs per collection
     dl_fmri_per_coll = colls.groupby("id").size()
@@ -96,11 +45,11 @@ def load_colls(neurovault,
     colls = colls.join(dl_fmri_per_coll)
 
     # Remove duplicates (1 row per coll instead of 1 per fMRI)
-    colls = colls[~colls.index.duplicated(keep='first')]
-
-    # Remove linebreaks in descriptions to better handle them as CSV afterwards
-    colls["description"] = colls["description"].replace("[\\n|\\r]", " ",
-                                                        regex=True)
+    colls = (
+        colls[~colls.index.duplicated(keep='first')]
+        .assign(description=lambda df: df["description"].replace("[\\n|\\r]", " ",
+                                                        regex=True))
+    )
 
     if verbose:
         print("  > Unique collections whose fMRIs were succesfully downloaded:",
@@ -108,8 +57,7 @@ def load_colls(neurovault,
         print("  > Collections with the most fMRIs:")
         print(colls[["number_of_images", "downloaded_images", "name"]]
               .sort_values("downloaded_images", ascending=False)
-              .head(10),
-              "\n")
+              .head(10))
 
     return colls
 
@@ -117,32 +65,29 @@ def load_colls(neurovault,
 def load_fmris(neurovault):
     """ Loads fMRIs metadata into a dataframe
 
-    :param nv_file: str
-        Pickle file where the full data is saved
-        (for faster loading than the fetch_neurovault)
-    :param fmris_file: str
-        CSV file  where to save fMRIs metadata
+    :param neurovault: nilearn.utils.Bunch
+        Bunch variable returned by the fetch_neurovault from nilearn
 
     :return: pandas.DataFrame
         Dataframe with all fMRIs metadata
     """
     # Build a dataframe from the fetcher's fMRIs metadata
     fmris = pd.DataFrame(neurovault.images_meta)
-    fmris.set_index("id", inplace=True)
-    fmris.sort_index(inplace=True)
 
-    # Remove linebreaks in descriptions to better handle them as CSV afterwards
-    fmris["description"] = fmris["description"].replace("[\\n|\\r]", " ",
-                                                        regex=True)
-
-    return fmris
+    return (
+        fmris.set_index("id")
+        .sort_index()
+        .assign(
+            description=lambda df: df["description"].replace("[\\n|\\r]", " ", regex=True)
+        )
+    )
 
 
 # ===========================
 # === ADDING TAGS FOR HCP ===
 # ===========================
 def get_hcp_tags(df, task, condition):
-    """ Gets the tags associated to a given task and condition for HCP
+    """Gets the tags associated to a given task and condition for HCP
 
     :param df: pandas.DataFrame
         Dataframe containing the associations.
@@ -159,24 +104,23 @@ def get_hcp_tags(df, task, condition):
     tags = df.columns[2:]
 
     # Get the array of indexes of the relevant tags
-    indexes = df[(df["Task"] == task)
-                 &
-                 (df["Condition"] == condition)].values[0, 2:]
-    indexes = indexes.astype(bool)
+    indexes = (
+        df[(df["Task"] == task) & (df["Condition"] == condition)]
+        .values[0, 2:]
+        .astype(bool)
+    )
 
     # Construct the comma-separated tags string
-    labels_str = ""
-    for tag in tags[indexes]:
-        labels_str = labels_str + tag + ","
-
-    return labels_str[:-1]
+    return ",".join(tags[indexes])
 
 
 def add_hcp_tags(fmris, hcp_file):
     # Load HCP labels and convert them to booleans
-    hcp_meta = pd.read_csv(hcp_file, sep='\t')
-    hcp_meta.replace(1.0, True, inplace=True)
-    hcp_meta.fillna(value=False, inplace=True)
+    hcp_meta = (
+        pd.read_csv(hcp_file, sep='\t')
+        .replace(1.0, True)
+        .fillna(value=False)
+    )
 
     # Get the tags string for each fMRI
     for idx, row in hcp_meta.iterrows():
@@ -217,9 +161,6 @@ def prepare_collect(global_config=None, verbose=False):
     hcp_file = global_config["collect"]["hcp_tags"]
     download_mode = global_config["collect"]["download_mode"]
 
-    # -----------------------------
-    # --- FETCH FROM NEUROVAULT ---
-    # -----------------------------
     if verbose:
         source = "disk" if download_mode == "offline" else "NeuroVault"
         print(f" > Fetch fMRIs from {source}")
@@ -233,19 +174,17 @@ def prepare_collect(global_config=None, verbose=False):
         verbose=2,
     )
 
-    neurovault_collections = load_colls(neurovault, colls_file, verbose)
+    if verbose:
+        print(" > Load NeuroVault metadata and images")
+
+    neurovault_collections = load_colls(neurovault, verbose)
     neurovault_fmris = load_fmris(neurovault)
 
     # Removal of Neurovault's first IBC version (reuploaded in better quality):
     FIRST_IBC_COLLECTION_TO_REMOVE = 2138
-
     fmris = neurovault_fmris.loc[lambda df: df.collection_id != FIRST_IBC_COLLECTION_TO_REMOVE]
-
     colls = neurovault_collections.drop(FIRST_IBC_COLLECTION_TO_REMOVE, axis=0)
 
-    # ------------------------------------------
-    # --- ADDING HCP TAGS FROM SEPARATE BASE ---
-    # ------------------------------------------
     if verbose:
         print(" > Adding tags from HCP")
 
