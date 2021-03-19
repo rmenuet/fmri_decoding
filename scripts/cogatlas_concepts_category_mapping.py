94324644eb61a4141c59ef57ba65d94b7ddfaf3a
Raphael Meudec <raphael.meudec@inria.fr>
2021-03-19 18:01:08 +0100

    Add docstrings

--- scripts/cogatlas_concepts_category_mapping.py
+++ scripts/cogatlas_concepts_category_mapping.py
@@ -1,4 +1,4 @@
-#%%
+
 import json
 import requests
 from functools import reduce
@@ -8,8 +8,10 @@ import pandas as pd
 import tqdm
 from bs4 import BeautifulSoup
 
-#%%
 def parse_mappings_from_categories_page():
+    """
+    Parse Cognitive Atlas Categories' page to a dict {concept: category}
+    """
     response = requests.get("https://www.cognitiveatlas.org/concepts/categories/all")
 
     content = BeautifulSoup(response.text, "html.parser")
@@ -31,33 +33,34 @@ def parse_mappings_from_categories_page():
 
 mapping_concept_to_category, mapping_category_to_concepts = parse_mappings_from_categories_page()
 
-#%%
 def get_cogatlas_concepts():
+    """
+    Fetch all cogatlas concepts as dict
+    """
     url = "https://www.cognitiveatlas.org/api/v-alpha/concept?format=json"
 
     return requests.get(url).json()
 
 cogatlas_concepts = get_cogatlas_concepts()
 
-#%%
 sample_labels_file = Path("../output") / "a7_label_all_syn_hyp.csv"
 labels = pd.read_csv(sample_labels_file)
 
-#%%
 unique_labels = reduce(
     lambda acc, el: acc.union(el),
     list(labels.labels_all.dropna().str.split(", ").values),
     set()
 )
 
-#%%
 missing_labels_in_mapping = [
     key for key in unique_labels
     if key not in mapping_concept_to_category.keys()
 ]
 
-#%%
 def find_cogatlas_concept_by_name(name, cogatlas_concepts):
+    """
+    Search a specific cogatlas concepts in the dict of all cogatlas concepts
+    """
     matches = [concept for concept in cogatlas_concepts if name == concept["name"]]
 
     if len(matches) == 0:
@@ -89,7 +92,6 @@ def fetch_cogatlas_parent_by_id(concept_id):
 
 analogy_id = find_cogatlas_concept_by_name("analogy", cogatlas_concepts)
 
-#%%
 potential_matched_concepts = {}
 for missing_concept in tqdm.tqdm(missing_labels_in_mapping):
     missing_concept_id = find_cogatlas_concept_by_name(missing_concept, cogatlas_concepts)
@@ -97,16 +99,13 @@ for missing_concept in tqdm.tqdm(missing_labels_in_mapping):
     if missing_concept_id is not None:
         potential_matched_concepts[missing_concept] = fetch_cogatlas_parent_by_id(missing_concept_id["id"])
 
-#%%
 for _ in range(3):
     for concept_name, matched_concept in potential_matched_concepts.items():
         if (concept_name not in mapping_concept_to_category) and (matched_concept in mapping_concept_to_category):
             mapping_concept_to_category[concept_name] = mapping_concept_to_category[matched_concept]
 
 
-#%%
 still_missing_labels = [label for label in missing_labels_in_mapping if label not in mapping_concept_to_category]
 
-#%%
 with open("cogatlas_concepts_categories_mapping.json", "w+") as file:
     json.dump(mapping_concept_to_category, file)
